44f6eeea239edc5654cd33a947e5ca135468ab2c
Oleg Goropai <oleg.goropai@refinitiv.com>
2025-02-19 13:58:07 +0200

    task02-004

--- task02/jsrc/main/java/com/task02/HelloWorld.java
+++ task02/jsrc/main/java/com/task02/HelloWorld.java
@@ -3,19 +3,13 @@ package com.task02;
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
 import com.amazonaws.services.lambda.runtime.events.APIGatewayV2HTTPEvent;
-import com.amazonaws.services.lambda.runtime.events.APIGatewayV2HTTPResponse;
-import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
 import com.syndicate.deployment.annotations.lambda.LambdaHandler;
-import com.syndicate.deployment.annotations.lambda.LambdaLayer;
 import com.syndicate.deployment.annotations.lambda.LambdaUrlConfig;
-import com.syndicate.deployment.model.Architecture;
-import com.syndicate.deployment.model.ArtifactExtension;
-import com.syndicate.deployment.model.DeploymentRuntime;
 import com.syndicate.deployment.model.RetentionSetting;
 import com.syndicate.deployment.model.lambda.url.AuthType;
 import com.syndicate.deployment.model.lambda.url.InvokeMode;
 
+import java.util.HashMap;
 import java.util.Map;
 import java.util.function.Function;
 
@@ -23,53 +17,52 @@ import java.util.function.Function;
     lambdaName = "hello_world",
 	roleName = "hello_world-role",
 	isPublishVersion = true,
-	aliasName = "${lambdas_alias_name}",
+	aliasName = /*"${lambdas_alias_name}"*/"learn",
 	logsExpiration = RetentionSetting.SYNDICATE_ALIASES_SPECIFIED
 )
-@LambdaLayer(
+/*@LambdaLayer(
 		layerName = "sdk-layer",
-		libraries = {"lib/commons-lang3-3.14.0.jar", "lib/gson-2.10.1.jar"},
+		libraries = {"lib/gson-2.11.0.jar"},
 		runtime = DeploymentRuntime.JAVA17,
 		architectures = {Architecture.ARM64},
 		artifactExtension = ArtifactExtension.ZIP
-)
+)*/
 @LambdaUrlConfig(
 		authType = AuthType.NONE,
 		invokeMode = InvokeMode.BUFFERED
 )
-public class HelloWorld implements RequestHandler<APIGatewayV2HTTPEvent, APIGatewayV2HTTPResponse> {
+public class HelloWorld implements RequestHandler<APIGatewayV2HTTPEvent, Map<String, Object>> {
 
 	private static final int SC_OK = 200;
 	private static final int SC_NOT_FOUND = 400;
-	private final Gson gson = new GsonBuilder().setPrettyPrinting().create();
 	private final Map<String, String> responseHeaders = Map.of("Content-Type", "application/json");
-	private final Map<RouteKey, Function<APIGatewayV2HTTPEvent, APIGatewayV2HTTPResponse>> routeHandlers = Map.of(
-			new RouteKey("GET", "/hello"), this::handleGetHello
+	private final Map<String, Function<APIGatewayV2HTTPEvent, Map<String, Object>>> routeHandlers = Map.of(
+			"GET#/hello", this::handleGetHello
 	);
 
 	@Override
-	public APIGatewayV2HTTPResponse handleRequest(APIGatewayV2HTTPEvent requestEvent, Context context) {
-		RouteKey routeKey = new RouteKey(getMethod(requestEvent), getPath(requestEvent));
-		return routeHandlers.getOrDefault(routeKey, this::notFoundResponse).apply(requestEvent);
+	public Map<String, Object> handleRequest(APIGatewayV2HTTPEvent requestEvent, Context context) {
+		String key = getMethod(requestEvent) + "#" + getPath(requestEvent);
+		return routeHandlers.getOrDefault(key, this::notFoundResponse).apply(requestEvent);
 	}
 
-	private APIGatewayV2HTTPResponse handleGetHello(APIGatewayV2HTTPEvent requestEvent) {
-		return buildResponse(SC_OK, Body.ok("Hello from Lambda"));
+	private Map<String, Object> handleGetHello(APIGatewayV2HTTPEvent requestEvent) {
+		return buildResponse(SC_OK, "Hello from Lambda");
 	}
 
-	private APIGatewayV2HTTPResponse notFoundResponse(APIGatewayV2HTTPEvent requestEvent) {
-		return buildResponse(SC_NOT_FOUND, Body.error("Bad request syntax or unsupported method. Request path: %s. HTTP method: %s".formatted(
+	private Map<String, Object> notFoundResponse(APIGatewayV2HTTPEvent requestEvent) {
+		return buildResponse(SC_NOT_FOUND,
+				"Bad request syntax or unsupported method. Request path: %s. HTTP method: %s".formatted(
 				getMethod(requestEvent),
 				getPath(requestEvent)
-		)));
+		));
 	}
 
-	private APIGatewayV2HTTPResponse buildResponse(int statusCode, Object body) {
-		return APIGatewayV2HTTPResponse.builder()
-				.withStatusCode(statusCode)
-				.withHeaders(responseHeaders)
-				.withBody(gson.toJson(body))
-				.build();
+	private Map<String, Object> buildResponse(int statusCode, Object message) {
+		Map<String, Object> resultMap = new HashMap<>();
+		resultMap.put("statusCode", statusCode);
+		resultMap.put("message", message);
+		return resultMap;
 	}
 
 	private String getMethod(APIGatewayV2HTTPEvent requestEvent) {
@@ -79,42 +72,4 @@ public class HelloWorld implements RequestHandler<APIGatewayV2HTTPEvent, APIGate
 	private String getPath(APIGatewayV2HTTPEvent requestEvent) {
 		return requestEvent.getRequestContext().getHttp().getPath();
 	}
-
-
-	private record RouteKey(String method, String path) {
-	}
-
-	private record Body(String message, String error) {
-		static Body ok(String message) {
-			return new Body(message, null);
-		}
-
-		static Body error(String error) {
-			return new Body(null, error);
-		}
-	}
-
-	/*public Map<String, Object> handleRequest(Object request, Context context) {
-		if (request instanceof APIGatewayProxyRequestEvent) {
-			APIGatewayProxyRequestEvent event = (APIGatewayProxyRequestEvent) request;
-			String path = event.getPath();
-			String method = event.getHttpMethod();
-			if ("/hello".equalsIgnoreCase(path) && "GET".equalsIgnoreCase(method)) {
-				System.out.println("Hello from lambda");
-				return generateResponse(200, "Hello from Lambda");
-			} else {
-				String errorMessage =
-                        "Bad request syntax or unsupported method. Request path: " + path + ". HTTP method: " + method;
-				return generateResponse(400, errorMessage);
-			}
-		}
-		return generateResponse(400, "Hello from Lambda");
-	}
-
-	private Map<String, Object> generateResponse(int statusCode, String message) {
-		Map<String, Object> resultMap = new HashMap<String, Object>();
-		resultMap.put("statusCode", statusCode);
-		resultMap.put("message", message);
-		return resultMap;
-	}*/
 }
