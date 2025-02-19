c104482a6a70cc89ce253286fe2e966fd12841b9
Oleg Goropai <oleg.goropai@refinitiv.com>
2025-02-19 10:19:39 +0200

    task02-001

--- task02/jsrc/main/java/com/task02/HelloWorld.java
+++ task02/jsrc/main/java/com/task02/HelloWorld.java
@@ -3,7 +3,6 @@ package com.task02;
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
 import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyRequestEvent;
-import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyResponseEvent;
 import com.syndicate.deployment.annotations.lambda.LambdaHandler;
 import com.syndicate.deployment.model.RetentionSetting;
 
@@ -20,22 +19,23 @@ import java.util.Map;
 public class HelloWorld implements RequestHandler<Object, Map<String, Object>> {
 
 	public Map<String, Object> handleRequest(Object request, Context context) {
-		String path = request.getPath();
-		String method = request.getHttpMethod();
-		if ("/hello".equalsIgnoreCase(path) && "GET".equalsIgnoreCase(method)) {
-			System.out.println("Hello from lambda");
-			return generateResponse(200, "Hello from Lambda");
-		} else {
-			String errorMessage =
-					"Bad request syntax or unsupported method. Request path: " +
-							path +
-							". HTTP method: " +
-							method;
-			return generateResponse(400, errorMessage);
+		if (request instanceof APIGatewayProxyRequestEvent) {
+			APIGatewayProxyRequestEvent event = (APIGatewayProxyRequestEvent) request;
+			String path = event.getPath();
+			String method = event.getHttpMethod();
+			if ("/hello".equalsIgnoreCase(path) && "GET".equalsIgnoreCase(method)) {
+				System.out.println("Hello from lambda");
+				return generateResponse(200, "Hello from Lambda");
+			} else {
+				String errorMessage =
+                        "Bad request syntax or unsupported method. Request path: " + path + ". HTTP method: " + method;
+				return generateResponse(400, errorMessage);
+			}
 		}
+		return generateResponse(400, "Hello from Lambda");
 	}
 
-	private Map<String, String> generateResponse(int statusCode, String message) {
+	private Map<String, Object> generateResponse(int statusCode, String message) {
 		Map<String, Object> resultMap = new HashMap<String, Object>();
 		resultMap.put("statusCode", statusCode);
 		resultMap.put("message", message);
