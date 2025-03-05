a32ba350b6ec9470fd6f66a045de824825eaac6a
Oleg Goropai <oleg.goropai@refinitiv.com>
2025-03-05 12:47:52 +0200

    task05-003

--- task05/jsrc/main/java/com/task05/ApiHandler.java
+++ task05/jsrc/main/java/com/task05/ApiHandler.java
@@ -1,12 +1,19 @@
 package com.task05;
 
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDB;
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClientBuilder;
+import com.amazonaws.services.dynamodbv2.model.AttributeValue;
+import com.amazonaws.services.dynamodbv2.model.PutItemResult;
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
 import com.syndicate.deployment.annotations.lambda.LambdaHandler;
 import com.syndicate.deployment.model.RetentionSetting;
 
+import java.time.ZonedDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.UUID;
 
 @LambdaHandler(
     lambdaName = "api_handler",
@@ -15,13 +22,40 @@ import java.util.Map;
 	aliasName = "${lambdas_alias_name}",
 	logsExpiration = RetentionSetting.SYNDICATE_ALIASES_SPECIFIED
 )
-public class ApiHandler implements RequestHandler<Object, Map<String, Object>> {
+public class ApiHandler implements RequestHandler<Map<String, Object>, Map<String, Object>> {
 
-	public Map<String, Object> handleRequest(Object request, Context context) {
-		System.out.println("Hello from lambda");
-		Map<String, Object> resultMap = new HashMap<String, Object>();
-		resultMap.put("statusCode", 200);
-		resultMap.put("body", "Hello from Lambda");
-		return resultMap;
+	private AmazonDynamoDB dynamoDB;
+
+	public ApiHandler(){
+		// initialize client
+		this.dynamoDB = AmazonDynamoDBClientBuilder.defaultClient();
+	}
+
+	@Override
+	public Map<String, Object> handleRequest(Map<String, Object> request, Context context) {
+		String uuid = UUID.randomUUID().toString();
+		String createdAt = ZonedDateTime.now().format(DateTimeFormatter.ISO_INSTANT);
+
+		// create map to store event attributes for DynamoDB
+		Map<String, AttributeValue> eventItem = new HashMap<>();
+		eventItem.put("id", new AttributeValue(uuid));
+		eventItem.put("principalId", new AttributeValue(request.get("principalId").toString()));
+		eventItem.put("createdAt", new AttributeValue(createdAt));
+		eventItem.put("body", new AttributeValue(request.get("content").toString()));  // make sure 'content' exists in the request
+
+		// put item in DynamoDB table
+		PutItemResult putItemResult = dynamoDB.putItem("Events", eventItem);
+
+		// prepare response
+		Map<String, Object> response = new HashMap<>();
+		response.put("statusCode", 201);
+		Map<String, Object> event = new HashMap<>();
+		event.put("id", uuid);
+		event.put("principalId", request.get("principalId"));
+		event.put("createdAt", createdAt);
+		event.put("body", request.get("content")); // here I assume that 'content' is properly parsed JSON object, not a string
+		response.put("event", event);
+
+		return response;
 	}
 }
