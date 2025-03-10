376ad3ba93706ce99734a7798f6abd3cc9dd0115
Oleg Goropai <oleg.goropai@refinitiv.com>
2025-03-10 12:41:43 +0200

    task06-001

--- task06/jsrc/main/java/com/task06/AuditProducer.java
+++ task06/jsrc/main/java/com/task06/AuditProducer.java
@@ -1,12 +1,23 @@
 package com.task06;
 
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClientBuilder;
+import com.amazonaws.services.dynamodbv2.model.GetItemRequest;
+import com.amazonaws.services.dynamodbv2.model.PutItemRequest;
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
+import com.amazonaws.services.lambda.runtime.events.DynamodbEvent;
+import com.amazonaws.services.dynamodbv2.model.AttributeValue;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.syndicate.deployment.annotations.environment.EnvironmentVariable;
+import com.syndicate.deployment.annotations.events.DynamoDbTriggerEventSource;
 import com.syndicate.deployment.annotations.lambda.LambdaHandler;
 import com.syndicate.deployment.model.RetentionSetting;
 
+import java.time.Instant;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.UUID;
 
 @LambdaHandler(
     lambdaName = "audit_producer",
@@ -15,13 +26,62 @@ import java.util.Map;
 	aliasName = "${lambdas_alias_name}",
 	logsExpiration = RetentionSetting.SYNDICATE_ALIASES_SPECIFIED
 )
-public class AuditProducer implements RequestHandler<Object, Map<String, Object>> {
+@EnvironmentVariable(
+		key = "target_table", value = "${target_table}"
+)
+@DynamoDbTriggerEventSource(
+		targetTable = "Configuration",
+		batchSize = 1
+)
+public class AuditProducer implements RequestHandler<DynamodbEvent, Void> {
+	private static final Gson GSON = new GsonBuilder().setPrettyPrinting().create();
 
-	public Map<String, Object> handleRequest(Object request, Context context) {
-		System.out.println("Hello from lambda");
-		Map<String, Object> resultMap = new HashMap<String, Object>();
-		resultMap.put("statusCode", 200);
-		resultMap.put("body", "Hello from Lambda");
-		return resultMap;
+	@Override
+	public Void handleRequest(DynamodbEvent event, Context context) {
+		context.getLogger().log("DynamoDB event received: " + GSON.toJson(event));
+		event.getRecords().forEach(rec -> processDynamoDBRecord(rec, context));
+		return null;
+	}
+
+	private void processDynamoDBRecord(DynamodbEvent.DynamodbStreamRecord record, Context context) {
+		context.getLogger().log("DynamoDB Record processed: " + GSON.toJson(record.getDynamodb()));
+
+		Map<String, com.amazonaws.services.lambda.runtime.events.models.dynamodb.AttributeValue> recordKeys
+				= record.getDynamodb().getKeys();
+		String updatedAt = Instant.now().toString();
+		if (recordKeys.containsKey("id")) {
+			String idValue = recordKeys.get("id").getS();
+			GetItemRequest request = new GetItemRequest()
+					.withTableName(System.getenv("target_table"))
+					.withKey(Map.of("id", new AttributeValue(idValue)));
+			Map<String, AttributeValue> existingItem = AmazonDynamoDBClientBuilder.defaultClient().getItem(request).getItem();
+			context.getLogger().log("Existing item: " + GSON.toJson(existingItem));
+
+			Map<String, AttributeValue> update = new HashMap<>();
+			update.put("id", new AttributeValue(UUID.randomUUID().toString()));
+			update.put("itemKey", new AttributeValue(record.getEventID()));
+			update.put("modificationTime", new AttributeValue(updatedAt));
+			update.put("updatedAttribute", new AttributeValue("value"));
+			update.put("oldValue", new AttributeValue().withN(existingItem.get("newValue").getM().get("value").getN()));
+			update.put("newValue", new AttributeValue().withN(record.getDynamodb().getNewImage().get("value").getN()));
+
+			putIntoAudit(update);
+		}
+		else {
+			Map<String, AttributeValue> item = new HashMap<>();
+			item.put("id", new AttributeValue(UUID.randomUUID().toString()));
+			item.put("itemKey", new AttributeValue().withN(String.valueOf(record.getEventID())));
+			item.put("modificationTime", new AttributeValue(updatedAt));
+			item.put("newValue", new AttributeValue().withS(GSON.toJson(record.getDynamodb())));
+
+			putIntoAudit(item);
+		}
+	}
+
+	private static void putIntoAudit(Map<String, AttributeValue> update) {
+		PutItemRequest putItemRequest = new PutItemRequest()
+				.withTableName("Audit")
+				.withItem(update);
+		AmazonDynamoDBClientBuilder.defaultClient().putItem(putItemRequest);
 	}
 }
