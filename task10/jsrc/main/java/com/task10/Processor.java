77168cc80b94d9008a4ec2b346f59a285d5abdac
Oleg Goropai <oleg.goropai@refinitiv.com>
2025-03-14 15:27:54 +0200

    task10-003

--- task10/jsrc/main/java/com/task10/Processor.java
+++ task10/jsrc/main/java/com/task10/Processor.java
@@ -2,6 +2,9 @@ package com.task10;
 
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
+import com.amazonaws.services.lambda.runtime.events.APIGatewayV2HTTPEvent;
+import com.google.gson.Gson;
+import com.google.gson.JsonObject;
 import com.syndicate.deployment.annotations.lambda.LambdaHandler;
 import com.syndicate.deployment.annotations.lambda.LambdaLayer;
 import com.syndicate.deployment.annotations.lambda.LambdaUrlConfig;
@@ -12,15 +15,19 @@ import com.syndicate.deployment.model.RetentionSetting;
 import com.syndicate.deployment.model.lambda.url.AuthType;
 import com.syndicate.deployment.model.lambda.url.InvokeMode;
 
+import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 
 @LambdaHandler(
-    lambdaName = "processor",
-	roleName = "processor-role",
-	isPublishVersion = true,
-	aliasName = "${lambdas_alias_name}",
-	logsExpiration = RetentionSetting.SYNDICATE_ALIASES_SPECIFIED
+		lambdaName = "processor",
+		roleName = "processor-role",
+		layers = "weather-api-layer",
+		isPublishVersion = true,
+		aliasName = "${lambdas_alias_name}",
+		runtime = DeploymentRuntime.JAVA11,
+		architecture = Architecture.ARM64,
+		logsExpiration = RetentionSetting.SYNDICATE_ALIASES_SPECIFIED
 )
 @LambdaLayer(
 		layerName = "weather-api-layer",
@@ -33,13 +40,27 @@ import java.util.Map;
 		authType = AuthType.NONE,
 		invokeMode = InvokeMode.BUFFERED
 )
-public class Processor implements RequestHandler<Object, Map<String, Object>> {
+public class Processor implements RequestHandler<APIGatewayV2HTTPEvent, Object> {
 
-	public Map<String, Object> handleRequest(Object request, Context context) {
-		System.out.println("Hello from lambda");
-		Map<String, Object> resultMap = new HashMap<String, Object>();
-		resultMap.put("statusCode", 200);
-		resultMap.put("body", "Hello from Lambda");
-		return resultMap;
+	public Object handleRequest(APIGatewayV2HTTPEvent request, Context context) {
+		context.getLogger().log("Request received: " + request);
+		String httpMethod = request.getRequestContext().getHttp().getMethod();
+		String path = request.getRequestContext().getHttp().getPath();
+		if ("GET".equalsIgnoreCase(httpMethod) && "/weather".equals(path)) {
+
+			App client = new App(52.52, 13.41);
+			try {
+				JsonObject weatherData = client.getWeatherForecast();
+				context.getLogger().log("Weather received: " + weatherData.toString());
+				return weatherData.toString();
+			} catch (IOException e) {
+				throw new RuntimeException(e);
+			}
+		}
+		else {
+			String errorMessage = String.format("Bad request syntax or unsupported method. " +
+					"Request path: %s. HTTP method: %s", path, httpMethod);
+			return new Gson().toJson(Map.of("statusCode", 400, "message", errorMessage));
+		}
 	}
 }
