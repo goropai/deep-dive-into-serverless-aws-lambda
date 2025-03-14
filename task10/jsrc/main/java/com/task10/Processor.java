defe263b98a2f0bca9845edb5ad49d5077b0c703
Oleg Goropai <oleg.goropai@refinitiv.com>
2025-03-14 17:11:03 +0200

    task10-006

--- task10/jsrc/main/java/com/task10/Processor.java
+++ task10/jsrc/main/java/com/task10/Processor.java
@@ -3,7 +3,6 @@ package com.task10;
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
 import com.amazonaws.services.lambda.runtime.events.APIGatewayV2HTTPEvent;
-import com.google.gson.Gson;
 import com.google.gson.JsonObject;
 import com.syndicate.deployment.annotations.environment.EnvironmentVariable;
 import com.syndicate.deployment.annotations.lambda.LambdaHandler;
@@ -14,7 +13,6 @@ import com.syndicate.deployment.model.lambda.url.AuthType;
 import com.syndicate.deployment.model.lambda.url.InvokeMode;
 
 import java.io.IOException;
-import java.util.Map;
 
 @LambdaHandler(
         lambdaName = "processor",
@@ -44,23 +42,14 @@ public class Processor implements RequestHandler<APIGatewayV2HTTPEvent, Object> 
 
     public Object handleRequest(APIGatewayV2HTTPEvent request, Context context) {
         context.getLogger().log("Request received: " + request);
-        String httpMethod = request.getRequestContext().getHttp().getMethod();
-        String path = request.getRequestContext().getHttp().getPath();
-        if ("GET".equalsIgnoreCase(httpMethod) && "/weather".equals(path)) {
-
-            App client = new App(52.52, 13.41);
-            try {
-                JsonObject weatherData = client.getWeatherForecast();
-                context.getLogger().log("Weather received: " + weatherData.toString());
-                persist.persistData(weatherData, System.getenv("target_table"), context.getLogger());
-                return weatherData.toString();
-            } catch (IOException e) {
-                throw new RuntimeException(e);
-            }
-        } else {
-            String errorMessage = String.format("Bad request syntax or unsupported method. " +
-                    "Request path: %s. HTTP method: %s", path, httpMethod);
-            return new Gson().toJson(Map.of("statusCode", 400, "message", errorMessage));
+        App client = new App(52.52, 13.41);
+        try {
+            JsonObject weatherData = client.getWeatherForecast();
+            context.getLogger().log("Weather received: " + weatherData.toString());
+            persist.persistData(weatherData, System.getenv("target_table"), context.getLogger());
+            return weatherData.toString();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
         }
     }
 }
